d56866a5ba866aecfca546f35e073ee3ab9f741a
Kimberly Aguilar <ae.xerx@gmail.com>
2024-09-25 13:24:25 +0800

    formula_for_celsius_to_fahrenheit

--- exercise_1_temperature_converter/try_exercise_1.py
+++ exercise_1_temperature_converter/try_exercise_1.py
@@ -2,4 +2,5 @@ temperature = int(input("Enter the temperature: "))
 type = str(input("Do you want to convert it from Celsius to Fahrenheit or from Fahrenheit to Celsius? "))
 
 if type == ("Celsius to Fahrenheit"):
+    converted_temperature = (1.8 * temperature + 32)
     
