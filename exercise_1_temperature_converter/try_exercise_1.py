0ff2f153910538b1f3091ddd924e2df1b9ff7906
Kimberly Aguilar <ae.xerx@gmail.com>
2024-09-25 13:22:34 +0800

    adding_if_and_else_statement

--- exercise_1_temperature_converter/try_exercise_1.py
+++ exercise_1_temperature_converter/try_exercise_1.py
@@ -1,2 +1,5 @@
 temperature = int(input("Enter the temperature: "))
 type = str(input("Do you want to convert it from Celsius to Fahrenheit or from Fahrenheit to Celsius? "))
+
+if type == ("Celsius to Fahrenheit"):
+    
